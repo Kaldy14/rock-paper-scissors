94284c6a6da0736c6a100d07e0e150b085379c8e
Kaldy <45780698+Kaldy@users.noreply.github.com>
2019-04-11 22:14:40 +0200

    Glowing things

--- other/script.js
+++ other/script.js
@@ -7,6 +7,7 @@ var paper_div = document.getElementById("paper");
 var scissors_div = document.getElementById("scissors");
 var result_p = document.querySelector(".result >p");
 
+var objType;
 var compPick;
 var userPick = "";
 
@@ -14,14 +15,18 @@ compChoice();
 
 function userChoice(){
     rock_div.addEventListener('click', function(){
+        getValue("rock");
         game("Rock");
         compChoice();
+        
     });
     paper_div.addEventListener('click', function(){
+        getValue("paper");
         game("Paper");
         compChoice();
     });
     scissors_div.addEventListener('click', function(){
+        getValue("scissors");
         game("Scissors");
         compChoice();
     });
@@ -42,20 +47,31 @@ function compChoice(){
     }
 }
 
+function getValue(oType){
+        objType = oType; 
+        console.log(objType);       
+}
+
 function win(){
     userScore++;
     userScore_span.innerHTML = userScore;
     result_p.innerHTML = " Computer picked " + compPick + ". You win!";
+    document.getElementById(objType).classList.add("green_glow");
+    setTimeout(function() {document.getElementById(objType).classList.remove("green_glow");},500);
 }
 
 function lost(){
     computerScore++;
     compScore_span.innerHTML = computerScore;
     result_p.innerHTML = "Computer picked " + compPick + ". You lost!";
+    document.getElementById(objType).classList.add("red_glow");
+    setTimeout(function() {document.getElementById(objType).classList.remove("red_glow");},500);
 }
  
 function draw(){
     result_p.innerHTML = "Computer picked " + compPick + ". It's a draw!";
+    document.getElementById(objType).classList.add("grey_glow");
+    setTimeout(function() {document.getElementById(objType).classList.remove("grey_glow");},500);
 }
 
 function game(choice1){
